4581a174b2545f2d8c9c385780796e62c77cdfdb
Wikan-dev <oneshootindo@gmail.com>
2026-05-14 12:16:03 +0800

    refractor: added more state to make it more reusable

--- src/components/common/SimplePill.tsx
+++ src/components/common/SimplePill.tsx
@@ -1,9 +1,14 @@
 import type { SimplePillProps } from "../../type/type";
 
-export default function SimplePill({ text }: SimplePillProps) {
+export default function SimplePill({ text, isSelected, onClick }: SimplePillProps) {
     return (
         <div>
-            <h1 className="px-6 py-3.5 xs-semibold text-text-label border-2 border-border-default rounded-2xl w-fit">{text}</h1>
+            <h1 
+                onClick={onClick}
+                className={`px-6 py-3.5 xs-semibold rounded-2xl w-fit transition-all duration-100 cursor-pointer ${isSelected ? 'border-2 border-text-action text-text-action bg-surface-default' : 'border-2 border-border-default text-text-label hover:border-border-action-hover'}`}
+            >
+                {text}
+            </h1>
         </div>
     )
 }
